d464c71d6fc7802e2b1a97ddbc586ef981bb9ff9
Alexander Ostrovsky <alexander@ostrovsky>
2024-06-18 22:12:35 +0200

    Update SimpleLCG to use uint32_t for seed

--- src/rand.h
+++ src/rand.h
@@ -10,33 +10,33 @@ namespace micro_nn::rand {
 template <typename NumT = config::kFloat>
 class SimpleLCG {
 public:
-    explicit constexpr SimpleLCG(std::uint64_t seed = time_to_int(__TIME__))
+    explicit constexpr SimpleLCG(std::uint32_t seed = time_to_int(__TIME__))
         : state_(seed) {}
 
     constexpr NumT next(NumT minInclusive, NumT maxExclusive) {
         assert(minInclusive < maxExclusive && "Invalid range");
-        state_ = (a_ * state_ + c_) % m;
-        return static_cast<NumT>(state_) / m * (maxExclusive - minInclusive) +
+        state_ = (a_ * state_ + c_) % m_;
+        return static_cast<NumT>(state_) / m_ * (maxExclusive - minInclusive) +
                minInclusive;
     }
 
 private:
-    constexpr static std::uint64_t time_to_int(const char* time) {
-        return (static_cast<std::uint64_t>(time[0] - '0') * 10 +
+    constexpr static std::uint32_t time_to_int(const char* time) {
+        return (static_cast<std::uint32_t>(time[0] - '0') * 10 +
                 (time[1] - '0')) *
                    3600 +
-               (static_cast<std::uint64_t>(time[3] - '0') * 10 +
+               (static_cast<std::uint32_t>(time[3] - '0') * 10 +
                 (time[4] - '0')) *
                    60 +
-               (static_cast<std::uint64_t>(time[6] - '0') * 10 +
+               (static_cast<std::uint32_t>(time[6] - '0') * 10 +
                 (time[7] - '0'));
     }
 
-    std::uint64_t state_;
+    std::uint32_t state_;
     // See https://en.wikipedia.org/wiki/Linear_congruential_generator for
     // explanation of a (multiplier), c (increment) and m (modulus)
     static constexpr uint32_t a_{1664525};
     static constexpr uint32_t c_{1013904223};
-    static constexpr uint32_t m{std::numeric_limits<uint32_t>::max()};
+    static constexpr uint32_t m_{std::numeric_limits<uint32_t>::max()};
 };
 }  // namespace micro_nn::rand
